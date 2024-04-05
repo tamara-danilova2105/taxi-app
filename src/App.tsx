f16c66a316171731ce56234a8bcbb8260565c731
tamara-danilova2105 <sap1028@mail.ru>
2024-04-05 06:55:48 +0400

    fix col-md-6 main page

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { MapApp } from "./components/MapApp/MapApp";
 
 function App() {
   return (
-    <Stack gap={3} className="col-md-5 mx-auto">
+    <Stack gap={3} className="col-md-6 mx-auto">
       <h2 className="mx-auto">Детали заказа</h2>
       <FormSearch />
       <Stack direction="horizontal" className="align-start" gap={3}>
